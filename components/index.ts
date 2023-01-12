64c4bb73e8615fd9f47ae7a58e0574333e441163
Angelo Leva <angelokta7@gmail.com>
2023-01-12 16:43:18 -0500

    feat(Membership & Resource): add styles

--- components/index.ts
+++ components/index.ts
@@ -6,6 +6,8 @@ export * from "./Header";
 export * from "./Hero";
 export * from "./JoinUs";
 export * from "./LessonCard";
+export * from "./Membership";
 export * from "./Nav";
+export * from "./Resource";
 export * from "./SocialIcon";
 export * from "./StudyGroup";
---
+++ models/resource.ts
@@ -0,0 +1,6 @@
+export interface IResource {
+  id: string;
+  link: string;
+  title: string;
+  session_id?: string;
+}
