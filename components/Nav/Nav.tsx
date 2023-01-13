b3b66611c096926506805d879b1da22adbc75af0
Angelo Leva <angelokta7@gmail.com>
2023-01-13 15:53:02 -0500

    feat(Nav): add alt default text

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -82,7 +82,7 @@ const Nav: React.FC<NavInterface> = () => {
                         <div className="nav__profile-photo">
                           <Image
                             src={user.photoURL}
-                            alt={user.displayName}
+                            alt={user.displayName || "User"}
                             width={50}
                             height={50}
                           />
